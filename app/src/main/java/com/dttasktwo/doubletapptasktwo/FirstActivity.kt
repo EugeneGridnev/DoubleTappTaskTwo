542c3fd60fda240adb79cda5e8e5f08c033cbf6b
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-13 21:21:32 +0300

    first activity added

--- app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
+++ app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
@@ -3,9 +3,9 @@ package com.dttasktwo.doubletapptasktwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_first)
     }
 }
