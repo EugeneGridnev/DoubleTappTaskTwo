a428d4a828d221b2ce6ec0dd1f3392a3d7c9d950
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-13 22:20:18 +0300

    first activity added

--- app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
+++ app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
@@ -2,15 +2,38 @@ package com.dttasktwo.doubletapptasktwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.dttasktwo.doubletapptasktwo.databinding.ActivityFirstBinding
+import kotlin.properties.Delegates.notNull
 
 class FirstActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityFirstBinding
 
-    lateinit var binding:ActivityFirstBinding
+    private var counter by notNull<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
+        binding = ActivityFirstBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
+        binding.firstButton.setOnClickListener {  }
 
+        counter = savedInstanceState?.getInt(COUNTER_KEY) ?: 0
+        binding.tvFirstNumber.text = counter.toString()
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        increment()
+        outState.putInt(COUNTER_KEY, counter)
+    }
+
+    private fun increment() {
+        counter++
+        binding.tvFirstNumber.text = counter.toString()
+    }
+
+    companion object {
+        @JvmStatic private val COUNTER_KEY = "COUNTER"
     }
 }
