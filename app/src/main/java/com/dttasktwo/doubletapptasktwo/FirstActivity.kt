509c91a081d8a39b95db54d94f4cd6a3078ca868
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-13 21:27:50 +0300

    binding added

--- app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
+++ app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
@@ -2,10 +2,15 @@ package com.dttasktwo.doubletapptasktwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dttasktwo.doubletapptasktwo.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityFirstBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+
     }
 }
