9b735ed92df8b704739f6b6a64af4c598e25b0c7
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-14 18:19:53 +0300

    logic realisation added

--- app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
+++ app/src/main/java/com/dttasktwo/doubletapptasktwo/FirstActivity.kt
@@ -1,5 +1,7 @@
 package com.dttasktwo.doubletapptasktwo
 
+import android.app.Activity
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,7 +17,7 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFirstBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        binding.firstButton.setOnClickListener {  }
+        binding.firstButton.setOnClickListener { onButtonPressed() }
 
         counter = savedInstanceState?.getInt(COUNTER_KEY) ?: 0
         binding.tvFirstNumber.text = counter.toString()
@@ -32,12 +34,18 @@ class FirstActivity : AppCompatActivity() {
         outState.putInt(COUNTER_KEY, counter)
     }
 
+    private fun onButtonPressed() {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra(COUNTER_KEY, counter)
+        startActivity(intent)
+    }
+
     private fun increment() {
         counter++
         binding.tvFirstNumber.text = counter.toString()
     }
 
     companion object {
-        @JvmStatic private val COUNTER_KEY = "COUNTER"
+        @JvmStatic val COUNTER_KEY = "COUNTER"
     }
 }
--- app/src/main/java/com/dttasktwo/doubletapptasktwo/SecondActivity.kt
+++ app/src/main/java/com/dttasktwo/doubletapptasktwo/SecondActivity.kt
@@ -1,17 +1,36 @@
 package com.dttasktwo.doubletapptasktwo
 
-import android.os.Bundle
-import android.os.PersistableBundle
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dttasktwo.doubletapptasktwo.FirstActivity.Companion.COUNTER_KEY
 import com.dttasktwo.doubletapptasktwo.databinding.ActivitySecondBinding
+import kotlin.math.pow
+import kotlin.properties.Delegates.notNull
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        binding = ActivitySecondBinding.inflate(layoutInflater).also {
-            setContentView(it.root)
-        }
+    private var sqr by notNull<Int>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        sqr = intent.getIntExtra(COUNTER_KEY,0)
+        binding.tvResult.text = sqr.toDouble().pow(power).toInt().toString()
+        binding.secondButton.setOnClickListener { onButtonPressed() }
+    }
+
+    private fun onButtonPressed() {
+        val intent = Intent(this, FirstActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+    }
+
+     companion object {
+        private const val power = 2
     }
 }
+
